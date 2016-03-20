b9068623d767df8eb94ba260e7d564c6990657a0
clem120 <clement.chouteau@gmail.com>
2016-03-20 17:39:45 +0100

    Ajoute de formula_io.hpp et formula_io.cpp

--- src/resol/main.cpp
+++ src/resol/main.cpp
@@ -1,11 +1,7 @@
 #include "formula.hpp"
 #include "clause.hpp"
 #include "prototype.hpp"
-/*
-#include "dpll_solver.hpp" //à virer dans un futur proche logiquement
-#include "../parser/formula_input.hpp"
-#include "../tseitin/tseitin.hpp"
-*/
+#include "formula_io.hpp"
 
 #include <iostream>
 #include <sstream>
@@ -15,8 +11,6 @@
 #include <utility>
 #include <cmath> // pour l'abs à la base. à virer
 #include <cstdlib> // sais pas
-#include <unistd.h> // dup2 dans treat_tseitin
-#include <fcntl.h> // open(2) dans treat_tseitin
 #include <map>
 #include <algorithm>
 
@@ -41,11 +35,6 @@ const static char HELP_OUTPUT[] =
 "The option --debug sets debug level to maximum\n"
 "\n"
 "Input file name must ends with .cnf or .for.\n";
-
-Formula treat_cnf(istream& is, const Option& option, ostream& os);
-//Formula treat_tseitin(string file, int debug, ostream& os);
-
-//Formula_input* parser(int debug, ostream& os);
 
 int main(int argc, char* argv[])
 {
@@ -143,7 +132,7 @@ int main(int argc, char* argv[])
 		f = treat_cnf(file, option, os);
 
 	else if (option.tseitin)
-        f = Formula(); //treat_tseitin(file_name, option.debug, cout);
+		f = treat_tseitin(file_name, option, cout);
 
 	else
 	{
@@ -178,125 +167,3 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
-
-/***********************************/
-/* Cas d'un fichier en forme cnf */
-/***********************************/
-bool is_commentary(const string& s)
-{
-	return s[0] == 'c';
-}
-
-bool is_end_of_clause(int x)
-{
-	return x == 0;
-}
-
-template <typename T> int sign(T val) {
-	return (T(0) < val) - (val < T(0));
-}
-
-Formula treat_cnf(istream& is, const Option& option, ostream& os)
-{
-    string line;
-    do
-    {	getline(is, line);	}
-    while (line[0] == 'c');
-    istringstream iss(line);
-
-	string p;
-    iss >> p;
-    if(p != "p")
-    {
-        cout << "Expected \"p\" on first line (" << p << " given)" << endl;
-        exit(0);
-    }
-
-    string cnf;
-    iss >> cnf;
-    if(cnf != "cnf")
-    {
-        cout << "Expected \"cnf\" after \"p\" (" << cnf << " given)" << endl;
-        exit(0);
-    }
-
-    unsigned int v,c;
-    if (!(iss >> v >> c))
-	{
-        cout << "Unable to get V and C" << endl;
-        exit(0);
-	}
-
-    unsigned int actual_v = 0, actual_c = 0;
-
-	list<Clause> clauses({});
-	vector<unsigned int> variables_inverse_mapping({0});
-	unsigned int next_available_var = 1;
-	map<int, unsigned int> variables_mapping({});
-
-	/* On récupère chaque ligne (c'est à dire chaque clause) */
-    while (getline(is, line))
-    {
-		if (is_commentary(line))
-            continue;
-
-        istringstream ss(line);
-		set<int> clause;
-		/* On récupère chaque littéral */
-        int x;
-        while (ss >> x)
-        {
-			if (is_end_of_clause(x))
-				break;
-
-			/* variable non déjà vue */
-			if (variables_mapping.find(abs(x)) == variables_mapping.end())
-			{
-				variables_inverse_mapping.push_back(abs(x));
-
-				variables_mapping[abs(x)] = next_available_var++;
-			}
-
-			/* On ajoute le nouveau littéral à la clause */
-			int mapped_x = sign(x) * variables_mapping[abs(x)]; // on remet le signe de x
-			clause.insert(mapped_x);
-
-			actual_v = max(actual_v, (unsigned int) abs(x));
-        }
-
-		/* On n'ajoute que les clauses bien terminées (terminées par 0) */
-        if (x == 0)
-        {
-            clauses.push_back(list<int>(clause.begin(),clause.end()));
-            actual_c++;
-        }
-		else
-		{
-			//!!!!!! affichage debug ici
-		}
-    }
-
-	Formula f(variables_mapping);
-	f.set_clauses_alive(clauses);
-
-    if (actual_v != v)
-        cout << "Expected " << v << " greatest variable (" << actual_v << " found)" << endl;
-
-    if (actual_c != c)
-        cout << "Expected " << c << " clauses (" << actual_c << " found)" << endl;
-
-	if (option.debug >= 1)
-        os << "Reading complete !" << endl << "Launching DPLL Solver..." << endl;
-	return f;
-}
-
-/*
-Formula treat_tseitin(string filename, int debug, ostream& os)
-{
-	int fd = open(filename.c_str(), O_RDONLY);
-	dup2(fd, 0);
-	Formula f = tseitin(*parser(debug, os), debug, os);
-
-	return f;
-}
-*/

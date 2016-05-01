ef1bad77056c5647fd26f79ec1131a10ff204d05
clem120 <clement.chouteau@gmail.com>
2016-05-01 13:23:45 +0200

    Ajout des fichiers Union_find

---
+++ src/resol/union_find.cpp
@@ -0,0 +1,63 @@
+#include "union_find.hpp"
+
+#include <cassert>
+
+using namespace std;
+
+Union_find::Union_find(UF::type t, unsigned int n) : type(t), weight(n, 0), father(n)
+{
+	for (unsigned int i = 0; i < n; i++)
+		father[i] = i;
+}
+
+void Union_find::merge(unsigned int i, unsigned int j)
+{
+	unsigned int p_i = rep(i);
+	unsigned int p_j = rep(j);
+
+	if (p_i == p_j)
+		return;
+
+	if (weight[p_i] < weight[p_j])
+	{
+		swap(i, j);
+		swap(p_i, p_j);
+	}
+
+	/* Maintenant w[p_i] >= w[p_j] */
+	if (type == UF::BACKTRACKING)
+	{
+		backtrack.push({p_i, p_j, weight[p_j]});
+	}
+
+	father[p_j] = p_i;
+	weight[p_i] += weight[p_j];
+}
+
+unsigned int Union_find::rep(unsigned int i) const
+{
+	// i = father[father...[i]] // heuristique ?
+
+	if (father[i] == i)
+		return i;
+	else
+		return rep(father[i]);
+}
+
+bool Union_find::equals(unsigned int i, unsigned int j) const
+{
+	return rep(i) == rep(j);
+}
+
+void Union_find::one_step_back()
+{
+	assert(type == UF::BACKTRACKING);
+
+	const Step& _ = backtrack.top();
+
+	father[_.p_j] = _.p_j;
+	weight[_.p_i] -= _.w_j;
+	weight[_.p_j] = _.w_j;
+
+	backtrack.pop();
+}
---
+++ src/resol/union_find.hpp
@@ -0,0 +1,44 @@
+#ifndef UNION_FIND_HPP
+#define UNION_FIND_HPP
+
+#include <vector>
+#include <stack>
+#include <utility>
+
+namespace UF
+{
+	enum type {NO_BACKTRACKING, BACKTRACKING};
+}
+
+class Union_find // gère la fusion avec poids mais pas la compression de chemin
+{
+public:
+	Union_find(UF::type, unsigned int n);
+
+	void merge(unsigned int, unsigned int);
+
+	unsigned int rep(unsigned int) const;
+
+	bool equals(unsigned int, unsigned int) const;
+
+	/* annule le dernier appel à merge(i, j) qui a eu une effet */
+	void one_step_back(); // l'appeler seulement si BACKTRACKING est activé
+
+
+private:
+	struct Step
+	{
+		unsigned int p_i; // plus gros
+		unsigned int p_j; // plus petit
+		unsigned int w_j; // ancien poids de p_j
+	};
+
+private:
+	const UF::type type;
+
+	std::stack< Step > backtrack;
+	std::vector<unsigned int> weight;
+	std::vector<unsigned int> father;
+};
+
+#endif // UNION_FIND_HPP
